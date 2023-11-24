423c0ae8dac6ea31fd4ed724f71e9f310531b789
Miguel Palhas <mpalhas@gmail.com>
2023-11-24 08:03:07 +0000

    hidapi-rusb

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,6 @@
-// use crate::{
-//     common::{APDUAnswer, APDUCommand},
-//     errors::LedgerError,
-//     transports::hid,
-
-// };
-mod hid;
-
 #[tokio::main]
 async fn main() -> anyhow::Result<()> {
-    hid::TransportNativeHID::new();
+    hidapi_rusb::HidApi::new();
 
     Ok(())
 }

299501fb21cc85d842f8fec1320b588382d66d88
Miguel Palhas <mpalhas@gmail.com>
2023-11-24 07:45:11 +0000

    cleanujp

--- src/main.rs
+++ src/main.rs
@@ -2,8 +2,7 @@ use coins_ledger::transports::LedgerAsync;
 
 #[tokio::main]
 async fn main() -> anyhow::Result<()> {
-    // let ledger = ethers_signers::Ledger::new(ethers_signers::HDPath::LedgerLive(0), 1).await;
-    let transport = coins_ledger::transports::Ledger::init()
+    coins_ledger::transports::Ledger::init()
         .await
         .expect("can't init ledger");
 
